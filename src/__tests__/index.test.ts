93051d62e30e09f106f3385fb700ecb8365611eb
Kevin Van Ryckegem <kevin@createmytech.com>
2022-01-23 11:32:01 +0100

    Single, and multi capture functionality, 100% covered

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,149 @@
-import { Capture } from '../index';
+import { CaptureOutput, SpeedyShotCapture } from '../index';
+import axios, { AxiosResponse } from 'axios';
 
-test('Speedyshot Capture', () => {
-  expect(Capture()).toBe('Welcome to SpeedyShot');
+const successAxiosResponse: AxiosResponse = {
+    status: 200,
+    statusText: 'success',
+    data: {
+        fileUrl: 'dummyUrl',
+    },
+    headers: {
+        'x-credits-remaining-before': '100',
+        'x-credits-refill-timestamp': '1642932140',
+        'x-api-call-id': 'xyz-123',
+        'content-type': 'application/json',
+        'content-length': '1200',
+        'x-ratelimit-limit': '100',
+        'x-ratelimit-remaining': '99',
+    },
+    config: {},
+};
+const successOutput: CaptureOutput = {
+    result: {
+        fileUrl: 'dummyUrl',
+    },
+    metaData: {
+        contentLength: 1200,
+        contentType: 'application/json',
+        creditsLeftBeforeRequest: 100,
+        creditsRefillTimestamp: 1642932140,
+        rateLimitMaxPerSecond: 100,
+        rateLimitRemaining: 99,
+        apiCallId: 'xyz-123',
+    },
+};
+
+describe('Given Speedyshot Capture', () => {
+    const apiKey = 'dummy-api-key';
+    const captureService = new SpeedyShotCapture(apiKey);
+    const axiosGetMock = jest.spyOn(axios, 'post');
+
+    describe('When using the single capture and it succeeds', () => {
+        test('Then we should get a result', async () => {
+            axiosGetMock.mockResolvedValueOnce(successAxiosResponse);
+            const result = await captureService.captureSingle({
+                url: 'dummy',
+                output: 'jpeg',
+            });
+            expect(captureService._maxConcurrency).toBe(50);
+            expect(result).toEqual(successOutput);
+        });
+    });
+
+    describe('When using the single capture with a ceiled maxConcurrency and it succeeds', () => {
+        const customCaptureService = new SpeedyShotCapture(apiKey, {
+            maxConcurrency: 101,
+        });
+        test('Then we should get a result', async () => {
+            axiosGetMock.mockResolvedValueOnce(successAxiosResponse);
+            const result = await customCaptureService.captureSingle({
+                url: 'dummy',
+                output: 'jpeg',
+            });
+            expect(customCaptureService._maxConcurrency).toBe(100);
+            expect(result).toEqual(successOutput);
+        });
+    });
+
+    describe('When using the single capture with all options and it succeeds', () => {
+        const customServiceUrl = 'https://myservice';
+        const customServiceEndpoint = '/api/custom/snap';
+        const maxConcurrency = 20;
+        const customCaptureService = new SpeedyShotCapture(apiKey, {
+            customServiceUrl,
+            customServiceEndpoint,
+            maxConcurrency,
+            includeRawResponse: true,
+        });
+
+        test('Then we should get a result', async () => {
+            axiosGetMock.mockResolvedValueOnce(successAxiosResponse);
+            const result = await customCaptureService.captureSingle({
+                url: 'dummy',
+                output: 'jpeg',
+            });
+
+            expect(customCaptureService._maxConcurrency).toBe(maxConcurrency);
+            expect(customCaptureService._serviceEndpoint).toBe(customServiceEndpoint);
+            expect(customCaptureService._serviceBaseUrl).toBe(customServiceUrl);
+            expect(result).toEqual({ ...successOutput, raw: successAxiosResponse });
+        });
+    });
+
+    describe('When using the single capture and it fails', () => {
+        test('Then we should get a failure', async () => {
+            const error = 'dummyerror';
+            axiosGetMock.mockImplementationOnce(() => {
+                throw new Error(error);
+            });
+            expect.assertions(1);
+            expect(
+                captureService.captureSingle({
+                    url: 'dummy',
+                    output: 'jpeg',
+                }),
+            ).rejects.toThrow(error);
+        });
+    });
+
+    describe('When using the multi capture and it succeeds', () => {
+        test('Then we should get multiple results', async () => {
+            axiosGetMock.mockResolvedValueOnce(successAxiosResponse);
+            axiosGetMock.mockResolvedValueOnce(successAxiosResponse);
+            const result = await captureService.captureMultiple([
+                {
+                    url: 'dummy',
+                    output: 'jpeg',
+                },
+                {
+                    url: 'dummy',
+                    output: 'jpeg',
+                },
+            ]);
+            expect(result).toEqual([successOutput, successOutput]);
+        });
+    });
+
+    describe('When using the multi capture and it fails', () => {
+        test('Then we should get a failure', async () => {
+            const error = 'dummyerror';
+            axiosGetMock.mockResolvedValueOnce(successAxiosResponse);
+            axiosGetMock.mockImplementationOnce(() => {
+                throw new Error(error);
+            });
+            expect.assertions(1);
+            expect(
+                captureService.captureMultiple([
+                    {
+                        url: 'dummy',
+                        output: 'jpeg',
+                    },
+                    {
+                        url: 'dummy',
+                        output: 'jpeg',
+                    },
+                ]),
+            ).rejects.toThrow(error);
+        });
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,113 @@
-export const Capture = () => `Welcome to SpeedyShot`;
+import pLimit, { Limit } from 'p-limit';
+import axios, { AxiosResponse } from 'axios';
+
+export interface CaptureParameters {
+    url: string;
+    output: 'jpeg' | 'webp' | 'png' | 'pdf' | 'text' | 'html';
+    quality?: number;
+    fullPage?: boolean;
+    clip?: CaptureClipParameter;
+    omitBackground?: boolean;
+    captureBeyondViewport?: boolean;
+    scale?: number;
+    printBackground?: boolean;
+    width?: number;
+    height?: number;
+    isMobile?: boolean;
+    hasTouch?: boolean;
+    isLandscape?: boolean;
+    pdfFormat?: 'a0' | 'a1' | 'a2' | 'a3' | 'a4' | 'a5' | 'a6' | 'Letter' | 'Legal' | 'Tabloid' | 'Ledger';
+}
+
+export interface CaptureClipParameter {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export interface CaptureOptions {
+    maxConcurrency?: number;
+    customServiceUrl?: string;
+    customServiceEndpoint?: string;
+    includeRawResponse?: boolean;
+}
+
+export interface CaptureOutput {
+    result: any;
+    metaData: CaptureOutputMetaData;
+    raw?: AxiosResponse;
+}
+
+export interface CaptureOutputMetaData {
+    creditsLeftBeforeRequest: number;
+    creditsRefillTimestamp: number;
+    apiCallId: string;
+    contentType: string;
+    contentLength: number;
+    rateLimitMaxPerSecond: number;
+    rateLimitRemaining: number;
+}
+
+export class SpeedyShotCapture {
+    _apiKey: string;
+    _maxConcurrency: number;
+    _pLimit: Limit;
+    _serviceBaseUrl: string = 'https://service.speedyshot.com';
+    _serviceEndpoint: string = '/api/snap';
+    _includeRawResponse: boolean = false;
+
+    constructor(apiKey: string, options: CaptureOptions = {}) {
+        this._apiKey = apiKey;
+        this._maxConcurrency = isNaN(Number(options.maxConcurrency)) ? 50 : Number(options.maxConcurrency);
+        if (this._maxConcurrency > 100) {
+            this._maxConcurrency = 100;
+        }
+        this._pLimit = pLimit(this._maxConcurrency);
+        if (options.customServiceUrl) {
+            this._serviceBaseUrl = options.customServiceUrl;
+        }
+        if (options.customServiceEndpoint) {
+            this._serviceEndpoint = options.customServiceEndpoint;
+        }
+        if (options.includeRawResponse) {
+            this._includeRawResponse = options.includeRawResponse;
+        }
+    }
+
+    captureSingle(parameters: CaptureParameters): Promise<CaptureOutput> {
+        return this._pLimit(() => {
+            return axios.post(this._serviceBaseUrl + this._serviceEndpoint, parameters, {
+                headers: {
+                    authorization: this._apiKey,
+                },
+            });
+        }).then((response: AxiosResponse) => {
+            return this._transformResponse(response);
+        });
+    }
+
+    captureMultiple(parametersList: CaptureParameters[]): Promise<CaptureOutput[]> {
+        const promisesList = parametersList.map((parameters) => {
+            return this.captureSingle(parameters);
+        });
+
+        return Promise.all(promisesList);
+    }
+
+    _transformResponse(response: AxiosResponse): CaptureOutput {
+        return {
+            result: response.data,
+            metaData: {
+                creditsLeftBeforeRequest: parseInt(response.headers['x-credits-remaining-before'], 10),
+                creditsRefillTimestamp: parseInt(response.headers['x-credits-refill-timestamp'], 10),
+                apiCallId: response.headers['x-api-call-id'],
+                contentType: response.headers['content-type'],
+                contentLength: parseInt(response.headers['content-length'], 10),
+                rateLimitMaxPerSecond: parseInt(response.headers['x-ratelimit-limit'], 10),
+                rateLimitRemaining: parseInt(response.headers['x-ratelimit-remaining'], 10),
+            },
+            raw: this._includeRawResponse ? response : undefined,
+        };
+    }
+}
